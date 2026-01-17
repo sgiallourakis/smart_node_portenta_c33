a40dfc87556729081a064054a83651a5cf9f0acf
stevengiallourakis <sgiallourakis3@gmail.com>
2026-01-17 07:00:13 -0500

    Created time/rtc.h and time/rtc.cpp

---
+++ src/time/rtc.cpp
@@ -0,0 +1,11 @@
+#include "rtc.h"
+
+uint32_t rtc_now() {
+    return 0;
+}
+
+bool is_new_day() {
+    // compare last_run vs current day
+    return false;
+
+}
---
+++ src/time/rtc.h
@@ -0,0 +1,5 @@
+#pragme once
+#include <stdint.h>
+
+uint32_t rtc_now();
+bool is_new_day();
